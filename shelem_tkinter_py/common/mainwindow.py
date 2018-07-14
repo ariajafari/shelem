feb7f0e4fb1215e575cfc91ed4091ae19ef9dc84
Mehran Zareh <mehran_zareh@yahoo.com>
2018-07-15 01:00:10 +0200

    nothing :)

--- shelem_tkinter_py/common/mainwindow.py
+++ shelem_tkinter_py/common/mainwindow.py
@@ -7,3 +7,12 @@ mehranzareh@gmail.com
 +39 3911084002
 """
 
+
+from tkinter import *
+
+class mainWindow(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.master.title('Shelem')
+        self.master.configure(background='green')
--- shelem_tkinter_py/shelem_tkinter.py
+++ shelem_tkinter_py/shelem_tkinter.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 00:16:22 2018
 
+@author: Mehran Zareh
+mehranzareh@gmail.com
++39 3911084002
+"""
+
+
+from common.mainwindow import mainWindow, Tk
+root = Tk(screenName='main window')
+app = mainWindow(root)
+#app.master.title('Shelem')
+
+
+app.mainloop()
